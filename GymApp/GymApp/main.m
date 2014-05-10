654a0603abe4fc30e4dc1ca69f1c3cba237c8963
mdreid <mdreid1992@gmail.com>
2014-05-09 21:23:01 -0400

    Made starting files and template

---
+++ GymApp/GymApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GymApp
+//
+//  Created by mdreid 1 on 5/9/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GAAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GAAppDelegate class]));
+    }
+}

8e8c499bd3633c3abcf42ad31a46814b6e29fb2e
mdreid <mdreid1992@gmail.com>
2014-05-10 23:18:18 -0400

    Started making Model

---
+++ GymApp/GymApp/GAListExerciseTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  GAListExerciseTableViewController.h
+//  GymApp
+//
+//  Created by mdreid 1 on 5/10/14.
+//  Copyright (c) 2014 mdreid 1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GAListExerciseTableViewController : UITableViewController
+
+@property (nonatomic) NSMutableArray *exercises;
+
+@end
